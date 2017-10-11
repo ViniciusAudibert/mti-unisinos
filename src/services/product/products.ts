51dca8c66a157c1b5593e584db8e97a105209179
Vinicius Audibert <vini.audibert@gmail.com>
2017-10-11 19:38:47 -0300

    remover untracked files

--- src/services/product/products.ts
+++ src/services/product/products.ts
@@ -6,70 +6,70 @@ const MACHINE_ONE: Machine = {
   description: 'Phmande 1000',
   local: 'Panquecas do Alemão',
   urlImage: '/assets/products/maquina01.jpg',
-  lon: -29.795004,
-  lat: -51.152100
+  lat: -29.795004,
+  lon: -51.152100
 }
 
 export const PRODUCTS: Product[] =
-    [
-        {
-            id: 1,
-            description: 'Coca-Cola 350ml',
-            machine: MACHINE_ONE,
-            quantity: 10
-          },
-          {
-            id: 2,
-            description: 'Água de Coco 100ml',
-            machine: MACHINE_ONE,
-            quantity: 2
-          },
-          {
-            id: 3,
-            description: 'Toddynho 100ml',
-            machine: MACHINE_ONE,
-            quantity: 10
-          },
-          {
-            id: 4,
-            description: 'Red Bull 275ml',
-            machine: MACHINE_ONE,
-            quantity: 6
-          },
-          {
-            id: 5,
-            description: 'Água 500ml s/ Gás',
-            machine: MACHINE_ONE,
-            quantity: 10
-          },
-          {
-            id: 6,
-            description: 'Cappucino 100ml',
-            machine: MACHINE_ONE,
-            quantity: 0
-          },
-          {
-            id: 7,
-            description: 'Café Extra Forte 100ml',
-            machine: MACHINE_ONE,
-            quantity: 0
-          },
-          {
-            id: 8,
-            description: 'Água 500ml c/ Gás',
-            machine: MACHINE_ONE,
-            quantity: 10
-          },
-          {
-            id: 9,
-            description: 'Brahama 1L',
-            machine: MACHINE_ONE,
-            quantity: 4
-          },
-          {
-            id: 10,
-            description: 'Chocolate 5 Stars 25g',
-            machine: MACHINE_ONE,
-            quantity: 21
-          },
-        ]
+  [
+    {
+      id: 1,
+      description: 'Coca-Cola 350ml',
+      machine: MACHINE_ONE,
+      quantity: 10
+    },
+    {
+      id: 2,
+      description: 'Água de Coco 100ml',
+      machine: MACHINE_ONE,
+      quantity: 2
+    },
+    {
+      id: 3,
+      description: 'Toddynho 100ml',
+      machine: MACHINE_ONE,
+      quantity: 10
+    },
+    {
+      id: 4,
+      description: 'Red Bull 275ml',
+      machine: MACHINE_ONE,
+      quantity: 6
+    },
+    {
+      id: 5,
+      description: 'Água 500ml s/ Gás',
+      machine: MACHINE_ONE,
+      quantity: 10
+    },
+    {
+      id: 6,
+      description: 'Cappucino 100ml',
+      machine: MACHINE_ONE,
+      quantity: 0
+    },
+    {
+      id: 7,
+      description: 'Café Extra Forte 100ml',
+      machine: MACHINE_ONE,
+      quantity: 0
+    },
+    {
+      id: 8,
+      description: 'Água 500ml c/ Gás',
+      machine: MACHINE_ONE,
+      quantity: 10
+    },
+    {
+      id: 9,
+      description: 'Brahama 1L',
+      machine: MACHINE_ONE,
+      quantity: 4
+    },
+    {
+      id: 10,
+      description: 'Chocolate 5 Stars 25g',
+      machine: MACHINE_ONE,
+      quantity: 21
+    },
+  ]
